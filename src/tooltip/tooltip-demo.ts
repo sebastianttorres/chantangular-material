7615c92e21be8e8a2088d62f2acfdd88ab92c91b
Angular CLI <angular-cli@angular.io>
2017-11-14 17:28:40 -0300

    chore: initial commit from @angular/cli

---
+++ src/tooltip/tooltip-demo.ts
@@ -0,0 +1,19 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+import {TooltipPosition} from '@angular/material';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'tooltip-demo',
+  templateUrl: 'tooltip-demo.html',
+  styleUrls: ['tooltip-demo.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class TooltipDemo {
+  position: TooltipPosition = 'below';
+  message: string = 'Here is the tooltip';
+  disabled = false;
+  showDelay = 0;
+  hideDelay = 1000;
+  showExtraClass = false;
+}
